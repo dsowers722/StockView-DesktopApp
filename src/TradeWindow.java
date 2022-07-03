8c4fc0b1226624ffbd04b2744609847e1d5bd50c
Daniel <danielsowers722@gmail.com>
2022-07-04 01:23:31 +0300

    Welcome message

--- src/TradeWindow.java
+++ src/TradeWindow.java
@@ -6,11 +6,14 @@ public class TradeWindow extends JFrame implements Runnable {
     Account activeAccount;
     DimensionAdjuster adjuster = new DimensionAdjuster();
     Font font = new Font(Font.MONOSPACED, Font.PLAIN, adjuster.adjustedFontSize(12));
-    //JLabel welcomeLabel = addLabel("Welcome, " + activeAccount.getUsername(), Color.CYAN, null);
-    JPanel westPanel = addPanel(Color.DARK_GRAY, new Dimension(adjuster.adjustedWidth(300), adjuster.adjustedHeight(800)));
-    JPanel eastPanel = addPanel(Color.DARK_GRAY, new Dimension(adjuster.adjustedWidth(300), adjuster.adjustedHeight(800)));
-    JPanel northPanel = addPanel(Color.BLACK, new Dimension(adjuster.adjustedWidth(1400), adjuster.adjustedHeight(100)));
-    JPanel southPanel = addPanel(Color.BLACK, new Dimension(adjuster.adjustedWidth(1400), adjuster.adjustedHeight(100)));
+    JPanel westPanel = addPanel(new Color(40, 45, 55),
+                                new Dimension(adjuster.adjustedWidth(300), adjuster.adjustedHeight(800)));
+    JPanel eastPanel = addPanel(new Color(40, 45, 55),
+                                new Dimension(adjuster.adjustedWidth(300), adjuster.adjustedHeight(800)));
+    JPanel northPanel = addPanel(new Color(40, 45, 55),
+                                 new Dimension(adjuster.adjustedWidth(1400), adjuster.adjustedHeight(80)));
+    JPanel southPanel = addPanel(new Color(40, 45, 55),
+                                 new Dimension(adjuster.adjustedWidth(1400), adjuster.adjustedHeight(80)));
 
     public TradeWindow(Account activeAccount) {
         this.activeAccount = activeAccount;
@@ -26,6 +29,11 @@ public class TradeWindow extends JFrame implements Runnable {
         this.getContentPane().setBackground(new Color(40, 45, 55));
         // Panels
         //northPanel.add(welcomeLabel);
+        northPanel.add(addLabel("Welcome, " + activeAccount.getUsername(),
+                                new Color(140, 235, 255),
+                                new Rectangle(adjuster.adjustedXBound(600), adjuster.adjustedYBound(15),
+                                              adjuster.adjustedWidth(1400), adjuster.adjustedHeight(100)),
+                                new Font(font.getFontName(), Font.ITALIC, adjuster.adjustedFontSize(22))));
         this.add(BorderLayout.EAST, eastPanel);
         this.add(BorderLayout.WEST, westPanel);
         this.add(BorderLayout.NORTH, northPanel);
@@ -42,7 +50,7 @@ public class TradeWindow extends JFrame implements Runnable {
         return panel;
     }
 
-    public JLabel addLabel(String text, Color color, Rectangle bounds) {
+    public JLabel addLabel(String text, Color color, Rectangle bounds, Font font) {
         JLabel label = new JLabel(text);
         label.setForeground(color);
         label.setBounds(bounds);
